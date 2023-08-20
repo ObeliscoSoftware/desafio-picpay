b0f0d1d45752154a2f12e6ce5d23656411376807
Leandro Barbosa <boyleandro_208@hotmail.com>
2023-08-20 00:53:13 -0300

    feat: criando endpoints PUT, POST e DELETE para Usuario

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # desafio-picpay
+- Abrir o postgresql e criar um database chamado picpaysimplificado
+
+- Altere o user e password com as suas credenciais
--- src/main/java/com/picpaysimplificado/controller/UsuarioController.java
+++ src/main/java/com/picpaysimplificado/controller/UsuarioController.java
@@ -3,7 +3,16 @@ package com.picpaysimplificado.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.picpaysimplificado.domain.Usuario;
@@ -11,13 +20,46 @@ import com.picpaysimplificado.repository.UsuarioRepository;
 
 
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-	@GetMapping("/usuarios")
+	@GetMapping
 	public List<Usuario> listar() {
 		return usuarioRepository.findAll();
+	}
+	
+	@GetMapping("/{usuarioId}")
+	public ResponseEntity<Usuario> buscar(@PathVariable Long usuarioId) {
+		return usuarioRepository.findById(usuarioId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Usuario adicionar(@RequestBody Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+	
+	@PutMapping("/{usuarioId}")
+	public ResponseEntity<Usuario> alterar(@PathVariable Long usuarioId, 
+			@RequestBody Usuario usuario){
+		if(!usuarioRepository.existsById(usuarioId)) {
+			return ResponseEntity.notFound().build();
+		}
+		usuario.setId(usuarioId);
+		return ResponseEntity.ok(usuarioRepository.save(usuario));
+	}
+	
+	@DeleteMapping("/{usuarioId}")
+	public ResponseEntity<Void> remover(@PathVariable Long usuarioId){
+		if(!usuarioRepository.existsById(usuarioId)) {
+			return ResponseEntity.notFound().build();
+		}
+		usuarioRepository.deleteById(usuarioId);
+		return ResponseEntity.noContent().build();
 	}
 }
